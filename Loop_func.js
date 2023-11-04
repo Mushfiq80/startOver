53f842dc921a080974627af95adb35f0985daf1c
Mushfiq80 <aurpon80@gmail.com>
2023-11-04 18:14:05 +0600

    reverse done

--- Loop_func.js
+++ Loop_func.js
@@ -3,6 +3,6 @@ var roastGiven = 0;
 for(roastGiven; roastGiven <20;roastGiven++){
     console.log("Served"+roastGiven);
     if(roastGiven == 10){
-        // break;
-    }
+        break;
+    } 
 }
